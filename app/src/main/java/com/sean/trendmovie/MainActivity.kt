86ffd09804bbf0365966188cf8f88db0063905ad
sean <sean.ts.lim@tiongnam.com.my>
2021-01-23 11:06:13 +0800

    init commit

---
+++ app/src/main/java/com/sean/trendmovie/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sean.trendmovie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
